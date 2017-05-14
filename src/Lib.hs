387d87ea985d468f848e177abf6abf1fd2dbd8e7
Andrew Newman <andrewfnewman@gmail.com>
2017-05-14 23:02:46 +1000

    Fix various off by one errors

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,6 +5,7 @@ module Lib
     , Point
     , LineSegment
     , distance
+    , allButLast
     , perpendicularDistance
     , splitAtMaxDistance
     , douglasPeucker
@@ -21,7 +22,7 @@ distance (x1,y1) (x2,y2) = sqrt(((x1 - x2) ^ 2) + ((y1 - y2) ^ 2))
 douglasPeucker :: Double -> Seq Point -> Seq Point
 douglasPeucker epsilon points
   | points == D.empty = D.empty
-  | dmax > epsilon = douglasPeucker epsilon left >< douglasPeucker epsilon right
+  | dmax > epsilon = allButLast(douglasPeucker epsilon left) >< douglasPeucker epsilon right
   | otherwise = first points <| Lib.last points <| D.empty
   where
       (left, right) = D.splitAt index points
@@ -38,13 +39,17 @@ perpendicularDistance p@(pX, pY) (a@(aX, aY), b@(bX, bY))
         u = ((pX - aX) * deltaX + (pY - aY) * deltaY) / (deltaX * deltaX + deltaY * deltaY)
 
 splitAtMaxDistance :: LineSegment -> Seq Point -> (Double, Int)
-splitAtMaxDistance ls =
-    D.foldlWithIndex (\(max, index) ni a -> if cp a ls > max then (cp a ls, ni) else (max, index)) (0.0, 0)
+splitAtMaxDistance ls points =
+    D.foldlWithIndex (\(max, index) ni a -> if cp a ls > max then (cp a ls, ni + 1) else (max, index)) (0.0, 0) (modPts points)
     where
         cp = perpendicularDistance
+        modPts points = allButLast $ D.drop 1 points
 
 last :: Seq a -> a
 last (viewr -> xs :> x) = x
+
+allButLast :: Seq a -> Seq a
+allButLast (viewr -> xs :> x) = xs
 
 first :: Seq a -> a
 first (viewl -> x :< xs) = x
