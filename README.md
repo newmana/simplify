7f4417d02c7ad4e16d10728491ef398307ce78ab
Andrew Newman <andrewfnewman@gmail.com>
2017-05-14 22:27:23 +1000

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# simplify
+* Implement https://www.cs.swarthmore.edu/~adanner/cs97/s08/pdf/speedingPeuckerDouglas.pdf
---
+++ src/Lib.hs
@@ -0,0 +1,53 @@
+{-# LANGUAGE ViewPatterns #-}
+
+module Lib
+    ( someFunc
+    , Point
+    , LineSegment
+    , distance
+    , perpendicularDistance
+    , splitAtMaxDistance
+    , douglasPeucker
+    ) where
+
+import           Data.Sequence as D
+
+type Point = (Double,Double)
+type LineSegment = (Point,Point)
+
+distance :: Point -> Point -> Double
+distance (x1,y1) (x2,y2) = sqrt(((x1 - x2) ^ 2) + ((y1 - y2) ^ 2))
+
+douglasPeucker :: Double -> Seq Point -> Seq Point
+douglasPeucker epsilon points
+  | points == D.empty = D.empty
+  | dmax > epsilon = douglasPeucker epsilon left >< douglasPeucker epsilon right
+  | otherwise = first points <| Lib.last points <| D.empty
+  where
+      (left, right) = D.splitAt index points
+      (dmax, index) = splitAtMaxDistance (first points, Lib.last points) points
+
+-- http://paulbourke.net/geometry/pointlineplane/DistancePoint.java
+perpendicularDistance :: Point -> LineSegment -> Double
+perpendicularDistance p@(pX, pY) (a@(aX, aY), b@(bX, bY))
+    | u < 0 = distance p a
+    | u > 1 = distance p b
+    | otherwise = distance p (aX + u * deltaX, aY + u * deltaY)
+    where
+        (deltaX, deltaY) = (bX - aX, bY - aY)
+        u = ((pX - aX) * deltaX + (pY - aY) * deltaY) / (deltaX * deltaX + deltaY * deltaY)
+
+splitAtMaxDistance :: LineSegment -> Seq Point -> (Double, Int)
+splitAtMaxDistance ls =
+    D.foldlWithIndex (\(max, index) ni a -> if cp a ls > max then (cp a ls, ni) else (max, index)) (0.0, 0)
+    where
+        cp = perpendicularDistance
+
+last :: Seq a -> a
+last (viewr -> xs :> x) = x
+
+first :: Seq a -> a
+first (viewl -> x :< xs) = x
+
+someFunc :: IO ()
+someFunc = putStrLn "someFunc"
