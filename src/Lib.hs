888fb852899aca71e1cd5e48eb537c70f6c81279
Andrew Newman <andrewfnewman@gmail.com>
2017-05-15 20:59:22 +1000

    Fix split - working

--- src/Lib.hs
+++ src/Lib.hs
@@ -33,30 +33,27 @@ perpendicularDistance p@(pX, pY) (a@(aX, aY), b@(bX, bY))
 douglasPeucker :: Double -> Seq Point -> Seq Point
 douglasPeucker epsilon points
   | points == D.empty = D.empty
-  | dmax >= epsilon = allButLast(douglasPeucker epsilon left) >< douglasPeucker epsilon right
+  | dmax > epsilon = douglasPeucker epsilon left >< allButFirst(douglasPeucker epsilon right)
   | otherwise = first points <| Lib.last points <| D.empty
   where
-      (left, right) = D.splitAt (index + 1) points
+      (left, right) = (D.take index points, D.drop (index - 1) points)
       (dmax, index) = splitAtMaxDistance points
 
--- Takes a sequence of points but ignores the first and last elements.
 splitAtMaxDistance :: Seq Point -> (Double, Int)
 splitAtMaxDistance points =
-    D.foldlWithIndex (\(max, index) ni a -> if cp a ls > max then (cp a ls, ni) else (max, index)) (0.0, 0) (modPts points)
+    D.foldlWithIndex (\(max, index) ni a -> if cp a ls > max then (cp a ls, ni + 1) else (max, index)) (0.0, D.length points) points
     where
         ls = (first points, Lib.last points)
         cp = perpendicularDistance
-        modPts points = allButLast $ D.drop 1 points
 
 last :: Seq a -> a
 last (viewr -> xs :> x) = x
 
-allButLast :: Seq a -> Seq a
-allButLast (viewr -> xs :> x) = xs
-allButLast (viewr -> xs) = empty
-
 first :: Seq a -> a
 first (viewl -> x :< xs) = x
+
+allButFirst :: Seq a -> Seq a
+allButFirst (viewl -> x :< xs) = xs
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
